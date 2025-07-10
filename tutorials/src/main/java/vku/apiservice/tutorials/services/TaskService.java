c7ce5d3716066963b97c62511155a6720c2ac228
Ngo Thanh Tung <tungnt@softech.vn>
2025-07-10 13:10:58 +0700

    Add task

--- tutorials/src/main/java/vku/apiservice/tutorials/services/TaskService.java
+++ tutorials/src/main/java/vku/apiservice/tutorials/services/TaskService.java
@@ -1,21 +1,11 @@
 package vku.apiservice.tutorials.services;
 
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import vku.apiservice.tutorials.dtos.CreateRoleDto;
 import vku.apiservice.tutorials.dtos.CreateTaskDto;
 import vku.apiservice.tutorials.entities.*;
-import vku.apiservice.tutorials.exceptions.HttpException;
-import vku.apiservice.tutorials.repositories.RoleRepository;
 import vku.apiservice.tutorials.repositories.TaskRepository;
-import vku.apiservice.tutorials.repositories.UserRepository;
-import vku.apiservice.tutorials.repositories.UserRoleRepository;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
