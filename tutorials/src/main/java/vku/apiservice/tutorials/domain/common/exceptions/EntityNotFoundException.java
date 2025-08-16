97cb530770710e9d30a6329671bf254c80db37b6
Ngo Thanh Tung <tungnt@softech.vn>
2025-08-16 10:44:33 +0700

    Update Exception

--- tutorials/src/main/java/vku/apiservice/tutorials/domain/common/exceptions/EntityNotFoundException.java
+++ tutorials/src/main/java/vku/apiservice/tutorials/domain/common/exceptions/EntityNotFoundException.java
@@ -1,5 +1,7 @@
 package vku.apiservice.tutorials.domain.common.exceptions;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) { super(message); }
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
 }
